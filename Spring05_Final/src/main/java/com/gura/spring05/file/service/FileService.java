b60d6dffcd03d5cfcec08fdb858c9e8ca7da4cd7
jyshin <sjy1218v@gmail.com>
2021-02-14 17:56:43 +0900

    spring05/file/* 요청 처리함

---
+++ Spring05_Final/src/main/java/com/gura/spring05/file/service/FileService.java
@@ -0,0 +1,15 @@
+package com.gura.spring05.file.service;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.web.servlet.ModelAndView;
+
+import com.gura.spring05.file.dto.FileDto;
+
+public interface FileService {
+	public void getList(HttpServletRequest request);
+	public void saveFile(FileDto dto, ModelAndView mView,
+			HttpServletRequest request);
+	public void getFileData(int num, ModelAndView mView);
+	public void deleteFile(int num, HttpServletRequest request);
+}
